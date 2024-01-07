14fb8dd3728ee1f632c4c59335c7f8582f24bf80
Małgorzata Korona <malgorzatakorona1@gmail.com>
2024-01-07 21:28:41 +0100

    Add hydra configuration

--- mnist_classifier/train_model.py
+++ mnist_classifier/train_model.py
@@ -5,44 +5,53 @@ from torch import nn, optim
 
 from models.model import MyAwesomeModel
 
-# from data import mnist
+import hydra
+import logging
+
+from hydra.utils import to_absolute_path
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+# @click.group()
+# def cli():
+#     """Command line interface."""
+#     pass
 
-@click.group()
-def cli():
-    """Command line interface."""
-    pass
+
+# @click.command()
+# @click.option("--lr", default=1e-3, help="learning rate to use for training")
+# @click.option("--batch_size", default=256, help="batch size to use for training")
+# @click.option("--num_epochs", default=20, help="number of epochs to train for")
+# @click.option(
+#     "--model_location", default="mnist_classifier/models/trained_models/", help="number of epochs to train for"
+# )
 
 
-@click.command()
-@click.option("--lr", default=1e-3, help="learning rate to use for training")
-@click.option("--batch_size", default=256, help="batch size to use for training")
-@click.option("--num_epochs", default=20, help="number of epochs to train for")
-@click.option(
-    "--model_location", default="mnist_classifier/models/trained_models/", help="number of epochs to train for"
-)
-def train(lr, batch_size, num_epochs, model_location):
+@hydra.main(config_path = "conf", config_name = "default_training_conf.yaml")
+def train(config):
     """Train a model on MNIST."""
-    print("Training day and night")
-    print("Learning rate:", lr)
-    print("Batch size:", batch_size)
-    print("Number of epochs:", num_epochs)
+
+    hparams = config.experiment.hyperparameters
+    log = logging.getLogger(__name__)
+
+    log.info("Training day and night")
+    log.info("Learning rate:", hparams.lr)
+    log.info("Batch size:", hparams.batch_size)
+    log.info("Number of epochs:", hparams.num_epochs)
 
     # TODO: Implement training loop here
     model = MyAwesomeModel().to(device)
-
-    train_data = torch.load("data/processed/processed_data_train.pt")
-    train_labels = torch.load("data/processed/processed_labels_train.pt")
+    train_data = torch.load(to_absolute_path("data/processed/processed_data_train.pt"))
+    train_labels = torch.load(to_absolute_path("data/processed/processed_labels_train.pt"))
 
     train_set = torch.utils.data.TensorDataset(train_data, train_labels)
-    trainloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    trainloader = torch.utils.data.DataLoader(train_set, batch_size=hparams.batch_size, shuffle=True)
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=lr)
+    optimizer = optim.Adam(model.parameters(), lr=hparams.lr)
     losses = []
-    for epoch in range(num_epochs):
+    for epoch in range(hparams.num_epochs):
         for images, labels in trainloader:
             optimizer.zero_grad()
 
@@ -56,16 +65,16 @@ def train(lr, batch_size, num_epochs, model_location):
             optimizer.step()
 
             # running_loss += loss.item()
-        print(f"Epoch {epoch} Loss {loss}")
+        log.info(f"Epoch {epoch} Loss {loss}")
         losses.append(loss.item())
 
-    plt.plot(list(range(num_epochs)), losses)
-    plt.savefig("reports/figures/training_curve.png")
+    # plt.plot(list(range(num_epochs)), losses)
+    # plt.savefig("reports/figures/training_curve.png")
 
-    torch.save(model, f"{model_location}/model.pt")
+    torch.save(model, to_absolute_path(f"{hparams.model_location}/model.pt"))
 
 
-cli.add_command(train)
+# cli.add_command(train)
 
 if __name__ == "__main__":
     train()
